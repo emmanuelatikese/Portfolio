fd9cd4c00396b567b54086112b98f44b9f3ffb6e
emmanuelatikese <104688960+emmanuelatikese@users.noreply.github.com>
2023-05-10 19:01:34 +0000

    add

--- my-app/src/components/header.js
+++ my-app/src/components/header.js
@@ -5,10 +5,14 @@ export default function Header (){
     return (
 
         <div className='HeaderContainer'>
+            <span id='PortTitle'>Snr.Ati</span>
 
-            <div>
-        
-
+            <div className='HeaderWrapper'>
+                <span>Home</span>
+                <span>About</span>
+                <span>Service</span>
+                <span>Skills</span>
+                <span>Contact</span>
             </div>
 
 
