fc593352ca52e66cb556e3da6d0d49845489851f
emmanuelatikese <104688960+emmanuelatikese@users.noreply.github.com>
2023-05-10 18:18:15 +0000

    add

---
+++ my-app/src/components/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header (){
+
+    return (
+
+        <div className='HeaderContainer'>
+
+            <div>
+        
+
+            </div>
+
+
+        </div>
+    )
+}
