17d458c77ca08e035c2d39fafeb05292978c2b97
emmanuelatikese <104688960+emmanuelatikese@users.noreply.github.com>
2023-05-13 21:38:52 +0000

    add

--- my-app/src/components/about.js
+++ my-app/src/components/about.js
@@ -1,36 +1,96 @@
-import React from 'react'
-import {motion, useAnimation} from 'framer-motion';
+import React, {useState, useEffect} from 'react'
+import {motion, useAnimation, useMotionValue, useTransform} from 'framer-motion';
+import {useInView} from 'react-intersection-observer'
 
 
 export default function About(){
 
-    const control = useAnimation()
+    const [hiset, setHiset] = useState(true)
 
+    const control = useAnimation()
+    const {ref, inView} = useInView({
+        
+    })
+
+    const AboutVariant = useAnimation()
+    const HrVariant = useAnimation()
+
+
+    useEffect(()=>{
+        if(inView){
+            AboutVariant.start(
+                {
+                    
+                    backgroundColor:'black',
+                    display:'flex',
+                    justifyContent:'center',
+                    transition:{
+                        duration:0.5,
+
+                    },
+                    y:0,
+                    
+                }
+                )
+                console.log('the inView is ', inView)
+
+                HrVariant.start({
+                    width:'60px',
+                    transition:{
+                        duration:0.5
+                    }
+                })
+
+
+        }
+        
+        if(!inView){
+
+            AboutVariant.start({
+                y:10,
+                display:'none'
+            })
+            HrVariant.start({
+                width:'1px',
+               
+            })
+        }
+
+        
+        }
+    
+        
+    ,[inView])
 
     return(
-        <div  className="AboutContainer">
-
+        <div  
+            ref={ref}
+        
+        className="AboutContainer">
+            
         <motion.p 
-         className='AboutTitle'>
+            
+            animate={AboutVariant}
+            className='AboutTitle'>
             About
          </motion.p>
 
-        <hr className='About-hr'/>
+        <motion.hr 
+        animate={HrVariant}
+        className='About-hr'/>
 
         <div className="AboutWrapper">
 
-            <h2>Hi</h2>
 
-            <div className='circle'>
-        <div className='EyeContainer'>
+        { hiset ? <h2>Hi</h2> :<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
+            <circle cx="50" cy="50" r="45" fill="white" />
+            <circle cx="35" cy="40" r="5" fill="black" class="blink" />
+            <circle cx="65" cy="40" r="5" fill="black" class="blink" />
+            <path d="M30 60 Q 50 80 70 60" stroke="black" fill="transparent" stroke-width="3" />
+            </svg>}
+            
 
-            <div className='leftEye'></div>
-            <div className='rightEye'></div>
-        </div>
-
-        <div className='smiley'></div>
-
-            </div>
+            
         
         <p className="AboutContent">
         I'm a software developer with two years of experience in creating innovative and user-friendly solutions for various domains. 
