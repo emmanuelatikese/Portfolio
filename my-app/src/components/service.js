4c50e9a81eb2006b24be344feac506e1d74fc547
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-05-27 11:31:51 +0000

    add

--- my-app/src/components/service.js
+++ my-app/src/components/service.js
@@ -1,13 +1,17 @@
 import React, {useEffect, useState} from 'react';
 import {motion, useAnimation} from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
-import {FaDev, FaReact } from 'react-icons/fa'
+
+
+import {FaDev, FaReact, FaGit } from 'react-icons/fa'
 import {MdDeveloperMode} from "react-icons/md"
 import {GoDeviceDesktop}  from 'react-icons/go'
 import SkillWrapper from './skillWrapper';
-import { IoLogoPython,IoLogoHtml5 } from "react-icons/io5";
-import {DiJavascript1} from "react-icons/di";
+import {IoLogoPython,IoLogoHtml5,IoLogoElectron} from "react-icons/io5";
+import {DiJavascript1, DiNodejs} from "react-icons/di";
 import {TbBrandTypescript, TbBrandReactNative} from "react-icons/tb"
+import {RiCss3Line} from "react-icons/ri";
+import {SiMongodb,SiDjango} from "react-icons/si";
 
 import Langs from './lang';
 
@@ -30,13 +34,14 @@ export default function Service(){
     const [hoverReact, setHoverReact] = useState(false); 
     const [hoverRN, setHoverRN] = useState(false);
     const [hoverHTML, setHoverHTML] = useState(false);
-    const [hoverCss, setHoverCss] = useState(false);
+
+    const [hoverCSS, setHoverCSS] = useState(false);
     const [hoverE, setHoverE] = useState(false);
     const [hoverMongo, setHoverMongo] = useState(false);
     const [hoverNode, setHoverNode] = useState(false);
     const [hoverGit, setHoverGit] = useState(false);
     const [hoverDjango, setHoverDjango] = useState(false);
-    const [hoverRedis, setHoverRedis] = useState(false);
+
 
     useEffect(()=>{
         if(inView){
@@ -175,8 +180,24 @@ export default function Service(){
         <Langs name="React.Js" ClassName="LangWrapperReact" IconsName="LangIconReact" setHover={setHoverReact} hover={hoverReact} backgroundColor="#61DBFB" color="white" Logo={FaReact}/>
         <Langs name="ReactNative.Js" ClassName="LangWrapperRN" IconsName="LangIconRN" setHover={setHoverRN} hover={hoverRN} backgroundColor="#0984e3" color="white" Logo={TbBrandReactNative}/>
         <Langs name="html" ClassName="LangWrapperHtml" IconsName="LangIconHtml" setHover={setHoverHTML} hover={hoverHTML} backgroundColor="white" color="#e7592e" Logo={IoLogoHtml5}/>
-    
+        
+
+
+        <Langs name="css" ClassName="LangWrapper" IconsName="LangIcon" setHover={setHoverCSS} hover={hoverCSS} backgroundColor="#0984e3" color="white" Logo={RiCss3Line}/>
+        <Langs name="electron.js" ClassName="LangWrapperJava" IconsName="LangIconJava" setHover={setHoverE} hover={hoverE} backgroundColor="#323330" color="#f0db4f" Logo={IoLogoElectron}/>
+        <Langs name="mongo" ClassName="LangWrapperTS" IconsName="LangIconTS" setHover={setHoverMongo} hover={hoverMongo} backgroundColor="white" color="#0984e3" Logo={SiMongodb}/>
+        <Langs name="node" ClassName="LangWrapperReact" IconsName="LangIconReact" setHover={setHoverNode} hover={hoverNode} backgroundColor="#61DBFB" color="white" Logo={DiNodejs}/>
+        <Langs name="git" ClassName="LangWrapperRN" IconsName="LangIconRN" setHover={setHoverGit} hover={hoverGit} backgroundColor="#0984e3" color="white" Logo={FaGit}/>
+        <Langs name="django" ClassName="LangWrapperHtml" IconsName="LangIconHtml" setHover={setHoverDjango} hover={hoverDjango} backgroundColor="white" color="#e7592e" Logo={SiDjango}/>
         </div>
+
+        <div className=' btnContainer'>
+
+        <a href='https://github.com/emmanuelatikese' className='projectBtn'>
+           My Projects
+        </a>
+        </div>
+
 
         </div>
     )
